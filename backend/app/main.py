3acecfc6b9548fb5b099484ee8587ab26ca81a59
vserra3192 <vserra3192@gmail.com>
2026-03-31 09:53:30 -0400

    updated main.py and __init__.py within backend to contain mock data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from . import menu_items
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello"}
+
+@app.get("/menu-items")
+def get_menu_items():
+    return menu_items
