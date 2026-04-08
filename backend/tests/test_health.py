1bbdd62f8d68c2f4bd5f35a1b6701aaa8b04d214
acmurdockUMASS <acmurdock@umass.edu>
2026-04-08 10:28:53 -0400

    Alex branch (#6)
    * Added github actions file
    
    * Fixed file typo
    
    * Added README
    Tweaked Docker Compose file
    Added gitignore
    
    * Fixed PR, formatting issues.
    
    * More formatting fixes
    
    * Fixed failing test case
    
    * ISort fix, maybe?
    
    * Updated main.py and test_health.py, ran isort and
    black, should all work.
    
    * Formatting issues, ran black and
    isort locally, both approve now
    
    * fixed "geet" instead of get on line 6 test_health
    
    * typo in main.py
    
    * Updated linter formating steps and bugs
    
    * updated pytest
    
    * modified backend/requirements-dev.in to include httpx
    
    * httpx in req-dev.txt
    
    * updated fastApi in req-dev.txt
    
    * *Hopefully* fixed pathing issues, for imports.
    Pytest should run as expected.
    
    * Ran linter and all other tests locally.
    
    * CI couldnt reach backend root, created pytest.ini to as helper
    
    * Fixed requirements issues causing uvicorn to not
    be found; removed unneeded requirements.
    
    * Fixed URL pathing in main.py, so it properly
    interacts with Nginx
    
    * Fixed routing in github actions file to match main
    
    * Unreverted Accidental changes
    
    * Found another old pathway
    
    * Commented out failing lines until end2end branch
    is merged.
    
    * More commenting out
    
    * Added install pip tools
    
    Co-authored-by: jperretta-umass <jperretta@umass.edu>
    
    ---------
    
    Co-authored-by: Alex Murdock <amurdock@umass.edu>
    Co-authored-by: vserra3192 <vserra3192@gmail.com>
    Co-authored-by: jperretta-umass <jperretta@umass.edu>

--- backend/tests/test_health.py
+++ backend/tests/test_health.py
@@ -1,6 +1,11 @@
 from fastapi.testclient import TestClient
+
 from app.main import app
 
+
 def test_health():
-    c = TestClient(app)
-    assert c.get("/api/health").json() == {"ok": True}
+    client = TestClient(app)
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {"ok": True}
