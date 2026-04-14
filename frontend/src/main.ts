fa19b05ed7ea6aaa4f295dd600d3865010fac817
gruiap9 <gruia@pascale.ro>
2026-04-13 22:23:12 -0400

    Add customer landing page prototype with routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,12 +8,15 @@ import DelivererPage from "./components/DelivererPage.vue";
 import SingleOrder from "./components/SingleOrder.vue";
 import HomeView from "./components/HomeView.vue"
 import ItemPage from "./components/ItemPage.vue"
+import CustomerLandingPage from "./components/CustomerLandingPage.vue";
+
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-       { path: '/', component: HomeView },
+        { path: '/', component: HomeView },
+        { path: "/customer", component: CustomerLandingPage },
         {path: "/DelivererPage", component: DelivererPage },
         {path: "/SingleOrder", component: SingleOrder },
         {path: "/ItemPage", component: ItemPage } 
