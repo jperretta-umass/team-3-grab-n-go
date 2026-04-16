d0af5085b423559d0aa1abacb7a92e36f35e25df
vserra3192 <vserra3192@gmail.com>
2026-04-15 22:32:37 -0400

    updated models.py to include a password_hash field to save users hashed passwords

--- backend/app/models.py
+++ backend/app/models.py
@@ -30,6 +30,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False, index=True)
     email = Column(String, unique=True, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
     phone_num = Column(String, nullable=True)
     has_deliverer_profile = Column(Boolean, nullable=False, default=False)
     deliverer_id = Column(
