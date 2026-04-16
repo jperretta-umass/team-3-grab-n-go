1f78ddd181df0e93f94cc328506e4bfe2234ade2
vserra3192 <vserra3192@gmail.com>
2026-04-15 22:32:02 -0400

    created a authSchema for seperation of registerRequests, LoginRequests and authResponse

---
+++ backend/app/auth_schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class RegisterRequest(BaseModel):
+    username: str = Field(min_length=1)
+    email: EmailStr
+    password: str = Field(min_length=6)
+    phone_num: str | None = None
+    is_deliverer: bool = False
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class AuthResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    is_deliverer: bool
