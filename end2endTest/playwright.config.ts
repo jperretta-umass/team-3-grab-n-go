228e01a7313304d746b8db086fc365ab8c13ac86
Alex Murdock <amurdock@umass.edu>
2026-04-23 11:23:40 -0400

    Fixed pathing in config

--- end2endTest/playwright.config.ts
+++ end2endTest/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://127.0.0.1',
     trace: 'on-first-retry',
   },
   //webServer: {
