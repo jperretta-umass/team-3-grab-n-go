0c53b4d36532cd4269d47a3f04c91a51c6fa8704
Alex Murdock <amurdock@umass.edu>
2026-04-23 07:16:23 -0400

    Fixed test to work with current frontend. Chagned test to run in bash (permission issue)

--- end2endTest/tests/smoke.spec.ts
+++ end2endTest/tests/smoke.spec.ts
@@ -2,5 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('homepage loads', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('body')).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'My Delivery App' }),
+  ).toBeVisible();
+  await expect(page.getByText('Welcome to the Home Page!')).toBeVisible();
 });
