13c5b518f434a507c81c4e3a83e91b195fbe17ec
Alex Murdock <amurdock@umass.edu>
2026-04-23 04:34:46 -0400

    Not the bad mega branch

--- end2endTest/playwright.config.ts
+++ end2endTest/playwright.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
     baseURL: 'http://127.0.0.1:5173',
     trace: 'on-first-retry',
   },
-  webServer: {
-    command: 'cd ../frontend && npm install && npm run dev -- --host 0.0.0.0 --port 5173',
-    url: 'http://127.0.0.1:5173',
-    reuseExistingServer: true,
-    timeout: 120_000,
-  },
+  //webServer: {
+    //command: 'cd ../frontend && npm install && npm run dev -- --host 0.0.0.0 --port 5173',
+    //url: 'http://127.0.0.1:5173',
+    //reuseExistingServer: true,
+    //timeout: 120_000,
+  //},
   projects: [
     {
       name: 'chromium',
