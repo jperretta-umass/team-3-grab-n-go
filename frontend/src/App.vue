7af0a7b64451cf5bc70a9b9f88e5f46691e671b5
huang-grace05 <gracehuang13579@gmail.com>
2026-04-23 09:35:44 -0400

    Login Form Implemented

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import LoginForm from "./components/LoginForm.vue";
 </script>
 
 <template>
   <div id="app">
     <h1>Grab-n-Go</h1>
     <HelloWorld msg="Welcome to Grab-n-Go" />
+    <LoginForm />
   </div>
 </template>
