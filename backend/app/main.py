62ef5546ba0377a03c01e57c371a3d896fe0d088
vserra3192 <vserra3192@gmail.com>
2026-04-26 19:53:35 -0400

    updated routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,8 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
