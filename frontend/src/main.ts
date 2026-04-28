d383f3fab8cedd3f33b782429d3fa1157608df39
vserra3192 <vserra3192@gmail.com>
2026-04-27 22:27:00 -0400

    Re routes to /Login if user is not logged in

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -18,15 +18,30 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomeView },
-        { path: "/CustomerLanding", component: CustomerLandingPage },
-        {path: "/DelivererPage", component: DelivererPage },
-        {path: "/SingleOrder", component: SingleOrder },
-        {path: "/ItemPage", component: ItemPage },
-        {path: "/Register", component: Register },
-        {path: "/Login", component: Login },
-        {path: "/DelivererLanding", component: DelivererLanding }
+        { path: "/Register", component: Register },
+        { path: "/Login", component: Login },
+
+        { path: "/CustomerLanding", component: CustomerLandingPage, meta: { requiresAuth: true, role: "customer" } },
+        { path: "/DelivererLanding", component: DelivererLanding, meta: { requiresAuth: true, role: "deliverer" } },
+        { path: "/DelivererPage", component: DelivererPage, meta: { requiresAuth: true, role: "deliverer" } },
+        { path: "/SingleOrder", component: SingleOrder, meta: { requiresAuth: true, role: "customer" } },
+        { path: "/ItemPage", component: ItemPage, meta: { requiresAuth: true, role: "deliverer" } },
     ]
 });
+
+router.beforeEach((route) => {
+    const auth = localStorage.getItem("auth");
+    const isLoggedIn = !!auth;
+  
+    if (route.meta.requiresAuth && !isLoggedIn) {
+      return "/Login";
+    }
+    //re routes to related landing page from /login if already logged in
+    /*if (isLoggedIn && (route.path === "/Login" || route.path === "/Register")) {
+      const user = JSON.parse(auth!);
+      return user.is_deliverer ? "/DelivererLanding" : "/CustomerLanding";
+    }*/
+  });
 
 const app = createApp(App);
 app.use(router)
