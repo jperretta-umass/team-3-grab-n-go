65a41d428e2d9e98d535fba2ffcbefc32dd39318
Rbach24 <ramabac2006@gmail.com>
2026-04-30 22:16:18 -0400

    fixed CI issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,9 +9,9 @@ from app.auth import router as auth_router
 from app.database import get_db
 from app.init_db import init_database
 from app.models import MenuItem, Order
+from app.payments import router as payments_router
 from app.routers import customer
 
-from app.payments import router as payments_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -37,6 +37,7 @@ app.include_router(auth_router)
 app.include_router(customer.router)
 
 app.include_router(payments_router)
+
 
 @app.get("/")
 def root():
