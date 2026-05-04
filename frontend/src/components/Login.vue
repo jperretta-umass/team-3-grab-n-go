c8fc84aa23d9d559de5422b893c3940c1a1ec1ff
vserra3192 <vserra3192@gmail.com>
2026-05-03 22:38:21 -0400

    Updated Login/Router to use AuthSession instead of AuthUser

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -55,7 +55,7 @@
 
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { getPostAuthRoute, saveAuthUser, type AuthUser } from '../utils/auth'
+import { getPostAuthRoute, saveAuthSession, type AuthSession } from '../utils/auth'
 
 const email = ref('')
 const password = ref('')
@@ -85,9 +85,9 @@ async function onSubmit() {
       return
     }
 
-    const authUser = data as AuthUser
-    saveAuthUser(authUser)
-    router.push(getPostAuthRoute(authUser))
+    const authSession = data as AuthSession
+    saveAuthSession(authSession)
+    router.push(getPostAuthRoute(authSession.user))
   } catch {
     error.value = 'Network error (is the backend running?)'
   } finally {
