c02728532635dd97b562c0181f979623d6216812
Colin Kirn <ckirn@umass.edu>
2026-05-07 10:38:32 -0400

    fixed all Ui errors and filter fucntion to properly display order items in deliverer popup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,13 @@
 <!-- Works like a container of all pages -->
-
+<script setup lang = "ts">
+import {onMounted} from 'vue';
+import {items, fetchMenuItems } from './components/displayScripts/menuItems.ts';
+onMounted(async () => {
+  if (items.value.length === 0) {
+    await fetchMenuItems()
+  }
+})
+</script>
 
 <template>
   <router-view />
